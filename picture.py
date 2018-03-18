0ebf846f6de301ec4e9f1597844e0521714cceb9
Wang Yu <1024394026@qq.com>
2018-03-18 14:07:38 +0800

    modify

--- picture.py
+++ picture.py
@@ -3,7 +3,7 @@ from urllib import request
 from bs4 import BeautifulSoup
 import re
 
-url = r'https://tieba.baidu.com/p/3834007469'
+url = r'https://www.zhihu.com/question/22918070'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}
 page = request.Request(url, headers=headers)
@@ -11,7 +11,7 @@ page_info = request.urlopen(page).read().decode('utf-8')
 soup = BeautifulSoup(page_info, 'html.parser')
 
 links = soup.find_all(
-    'img', "BDE_Image", src=re.compile(r'.jpg$'))
+    'img', "origin_image zh-lightbox-thumb", src=re.compile(r'.jpg$'))
 
 local_path = r'D:\work\pachong\content\pic'
 

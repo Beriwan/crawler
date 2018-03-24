71c3a7ef7dfb6552abd5e14d4a7db3b317c920f0
Wang Yu <1024394026@qq.com>
2018-03-24 15:19:33 +0800

    Ajax + MongoDB

---
+++ Ajaxtest/config.py
@@ -0,0 +1,7 @@
+MONGO_URL = 'localhost'
+MONGO_DB = 'toutiao'
+MONGO_TABLE = 'toutiao'
+
+KEYWORD = '街拍美女'
+START = 0
+END = 21
--- Ajaxtest/spider.py
+++ Ajaxtest/spider.py
@@ -1,8 +1,17 @@
 import json
+import os
+import re
 import requests
+import pymongo
+from hashlib import md5
 from urllib.parse import urlencode
 from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
+from config import *
+
+client = pymongo.MongoClient(MONGO_URL)
+db = client[MONGO_DB]
 
 
 def get_page_index(offset, keyword):
@@ -47,21 +56,66 @@ def get_page_detail(url):
         return None
 
 
-def parse_page_detail(html):
-    soup = BeautifulSoup(html, 'html.parser')
+def parse_page_detail(html, url):
+    soup = BeautifulSoup(html, 'lxml')
     title = soup.select('title')[0].get_text()
     print(title)
+    images_pattern = re.compile('gallery: JSON.parse\((.*?)\),', re.S)
+    result = re.search(images_pattern, html)
+    if result:
+        data = json.loads(json.loads(result.group(1)))
+        if data and 'sub_images' in data.keys():
+            sub_images = data.get('sub_images')
+            images = [item.get('url') for item in sub_images]
+            for image in images:
+                download_image(image)
+            return {
+                'title': title,
+                'url': url,
+                'images': images
+            }
 
 
-def main():
-    html = get_page_index(0, '街拍')
+def save_to_mongo(result):
+    if db[MONGO_TABLE].insert(result):
+        print('存储到MongoDB')
+        return True
+    return False
+
+
+def download_image(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print('正在下载', url)
+            save_image(response.content)
+        return None
+    except RequestException:
+        print('请求图片出错')
+        return None
+
+
+def save_image(content):
+    file_path = '{0}/{1}.{2}'.format('D:\work\crawler\Ajaxtest\picture', md5(content).hexdigest(), 'jpg')
+    if not os.path.exists(file_path):
+        with open(file_path, 'wb') as f:
+            f.write(content)
+            f.close()
+
+
+def main(offset):
+    html = get_page_index(offset, KEYWORD)
     for url in parse_page_index(html):
         print(url)
         html = get_page_detail(url)
         if html:
-            #print(html)
-            parse_page_detail(html)
+            # print(html)
+            result = parse_page_detail(html, url)
+            if result:
+                save_to_mongo(result)
 
 
 if __name__ == '__main__':
-    main()
+    groups = [x * 20 for x in range(START, END)]
+    pool = Pool()
+    pool.map(main, groups)
